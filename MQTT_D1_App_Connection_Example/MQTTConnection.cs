4268fec8291edde3f1e4af3b836731f068c47ba6
sdamman <stewartodamman@gmail.com>
2023-05-15 13:45:46 -0500

    remove Newtonsoft library
    -JSON support is built into .NET, Newtonsoft library is not needed.

--- MQTT_D1_App_Connection_Example/MQTTConnection.cs
+++ MQTT_D1_App_Connection_Example/MQTTConnection.cs
@@ -3,150 +3,139 @@ using MQTTnet.Client;
 using MQTTnet.Extensions.ManagedClient;
 using MQTTnet.Formatter;
 using MQTTnet.Protocol;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using System.Text;
+using System.Text.Json;
 using MQTTnet.Server;
-using ErrorEventArgs = Newtonsoft.Json.Serialization.ErrorEventArgs;
 
 
 namespace MQTT_D1_App_Connection_Example;
 
 internal class MQTTConnection
 {
-    private IManagedMqttClient? managedMqttClient;
-    public event EventHandler<JSON_Response>? D1Response;
-    public event EventHandler<JSON_D1Readings>? D1Readings;
+  private IManagedMqttClient managedMqttClient;
+  public event EventHandler<JSON_Response> D1Response;
+  public event EventHandler<JSON_D1Readings> D1Readings;
 
 
-    public async Task<bool> StartClient()
+  public async Task<bool> StartClient()
+  {
+    managedMqttClient = new MqttFactory().CreateManagedMqttClient();
+
+    var tlsOptions = new MqttClientTlsOptions
     {
-        managedMqttClient = new MqttFactory().CreateManagedMqttClient();
+      UseTls = false,
+      IgnoreCertificateChainErrors = true,
+      IgnoreCertificateRevocationErrors = true,
+      AllowUntrustedCertificates = true
+    };
 
-        var tlsOptions = new MqttClientTlsOptions
+    var options = new MqttClientOptions
+    {
+      CleanSession = true,
+      ClientId = "ConsoleApp",
+      KeepAlivePeriod = TimeSpan.FromSeconds(5),
+      ProtocolVersion = MqttProtocolVersion.V500,
+      ChannelOptions = new MqttClientTcpOptions
+      {
+        Server = "localhost",
+        Port = 1883,
+        TlsOptions = tlsOptions
+      }
+    };
+
+    await managedMqttClient.SubscribeAsync("d1-readings", MqttQualityOfServiceLevel.AtLeastOnce);
+    await managedMqttClient.SubscribeAsync("d1-response", MqttQualityOfServiceLevel.AtLeastOnce);
+
+    await managedMqttClient.StartAsync(
+        new ManagedMqttClientOptions
         {
-            UseTls = false,
-            IgnoreCertificateChainErrors = true,
-            IgnoreCertificateRevocationErrors = true,
-            AllowUntrustedCertificates = true
-        };
+          ClientOptions = options
+        });
 
-        var options = new MqttClientOptions
-        {
-            CleanSession = true,
-            ClientId = "ConsoleApp",
-            KeepAlivePeriod = TimeSpan.FromSeconds(5),
-            ProtocolVersion = MqttProtocolVersion.V500,
-            ChannelOptions = new MqttClientTcpOptions
-            {
-                Server = "localhost",
-                Port = 1883,
-                TlsOptions = tlsOptions
-            }
-        };
+    Thread.Sleep(1000); //The client needs time to sort itself (approx 0.125s on development machine)
+    managedMqttClient.ApplicationMessageReceivedAsync += HandleReceivedApplicationMessage;
 
-        await managedMqttClient.SubscribeAsync("d1-readings", MqttQualityOfServiceLevel.AtLeastOnce);
-        await managedMqttClient.SubscribeAsync("d1-response", MqttQualityOfServiceLevel.AtLeastOnce);
+    if (managedMqttClient.IsConnected == false) Console.WriteLine("Ran into an issue in connecting to the D1 App.");
+    else Console.WriteLine("Successfully Connected to the D1 App MQTT Server");
 
-        await managedMqttClient.StartAsync(
-            new ManagedMqttClientOptions
-            {
-                ClientOptions = options
-            });
+    return managedMqttClient.IsConnected;
+  }
 
-        Thread.Sleep(1000); //The client needs time to sort itself (approx 0.125s on development machine)
-        managedMqttClient.ApplicationMessageReceivedAsync += HandleReceivedApplicationMessage;
 
-        if (managedMqttClient.IsConnected == false) Console.WriteLine("Ran into an issue in connecting to the D1 App.");
-        else Console.WriteLine("Successfully Connected to the D1 App MQTT Server");
+  private Task ManagedMqttClient_ConnectedAsync(MqttClientConnectedEventArgs arg)
+  {
+    return Task.CompletedTask;
+  }
 
-        return managedMqttClient.IsConnected;
+  private Task ManagedMqttClient_DisconnectedAsync(MqttClientDisconnectedEventArgs arg)
+  {
+    return Task.CompletedTask;
+  }
+
+  public bool GetConnection()
+  {
+    return managedMqttClient.IsConnected;
+  }
+
+  public Task HandleReceivedApplicationMessage(MqttApplicationMessageReceivedEventArgs eventArgs)
+  {
+    var thisTopic = eventArgs.ApplicationMessage?.Topic;
+    var thisPayload = eventArgs.ApplicationMessage.ConvertPayloadToString();
+
+    var errors = new List<string>();
+
+    try
+    {
+      switch (thisTopic)
+      {
+        case "d1-response":
+          var d1Response = JsonSerializer.Deserialize<JSON_Response>(thisPayload);
+          if (!errors.Any())
+            D1Response.Invoke(this, d1Response);
+          break;
+        case "d1-readings":
+          var d1Reading = JsonSerializer.Deserialize<JSON_D1Readings>(thisPayload);
+          if (!errors.Any())
+            D1Readings.Invoke(this, d1Reading);
+          break;
+        default:
+          Console.WriteLine("Unknown topic");
+          break;
+      }
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine(ex.ToString() + "tried to read data from MQTT");
     }
 
+    return Task.CompletedTask;
+  }
 
-    private Task ManagedMqttClient_ConnectedAsync(MqttClientConnectedEventArgs arg)
+  public async Task<bool> MqttPublish(string topic, string payload)
+  {
+    try
     {
-        return Task.CompletedTask;
-    }
+      if (managedMqttClient == null)
+      {
+        return false;
+      }
 
-    private Task ManagedMqttClient_DisconnectedAsync(MqttClientDisconnectedEventArgs arg)
+      if (!managedMqttClient.IsConnected)
+      {
+        return false;
+      }
+
+      var thisPayload = Encoding.UTF8.GetBytes(payload);
+      var message = new MqttApplicationMessageBuilder()
+          .WithTopic(topic).WithPayload(payload)
+          .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce).WithRetainFlag().Build();
+
+      if (managedMqttClient != null) await managedMqttClient.EnqueueAsync(message);
+      return true;
+    }
+    catch (Exception)
     {
-        return Task.CompletedTask;
+      return false;
     }
-
-    public bool GetConnection()
-    {
-        return managedMqttClient.IsConnected;
-    }
-
-    public Task HandleReceivedApplicationMessage(MqttApplicationMessageReceivedEventArgs eventArgs)
-    {
-        var thisTopic = eventArgs.ApplicationMessage?.Topic;
-        var thisPayload = eventArgs.ApplicationMessage.ConvertPayloadToString();
-
-
-        var errors = new List<string>();
-        JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings
-        {
-            Error = delegate(object sender, ErrorEventArgs e)
-            {
-                errors.Add(e.ErrorContext.Error.Message);
-                e.ErrorContext.Handled = true;
-            }
-        };
-        var settings = jsonSerializerSettings;
-        try
-        {
-            switch (thisTopic)
-            {
-                case "d1-response":
-                    var d1Response = JsonConvert.DeserializeObject<JSON_Response>(thisPayload, settings);
-                    if (!errors.Any())
-                        D1Response.Invoke(this, d1Response);
-                    break;
-                case "d1-readings":
-                    var d1Reading = JsonConvert.DeserializeObject<JSON_D1Readings>(thisPayload, settings);
-                    if (!errors.Any())
-                        D1Readings.Invoke(this, d1Reading);
-                    break;
-                default:
-                    Console.WriteLine("Unknown topic");
-                    break;
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString() + "tried to read data from MQTT");
-        }
-
-        return Task.CompletedTask;
-    }
-
-    public async Task<bool> MqttPublish(string topic, string payload)
-    {
-        try
-        {
-            if (managedMqttClient == null)
-            {
-                return false;
-            }
-
-            if (!managedMqttClient.IsConnected)
-            {
-                return false;
-            }
-
-            var thisPayload = Encoding.UTF8.GetBytes(payload);
-            var message = new MqttApplicationMessageBuilder()
-                .WithTopic(topic).WithPayload(payload)
-                .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce).WithRetainFlag().Build();
-
-            if (managedMqttClient != null) await managedMqttClient.EnqueueAsync(message);
-            return true;
-        }
-        catch (Exception ex)
-        {
-            return false;
-        }
-    }
+  }
 }
--- MQTT_D1_App_Connection_Example/Program.cs
+++ MQTT_D1_App_Connection_Example/Program.cs
@@ -1,67 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using Newtonsoft.Json;
+using System.Text.Json;
 using MQTT_D1_App_Connection_Example;
 
-MQTTConnection mqtt = new MQTTConnection();
+MQTTConnection mqtt = new();
 mqtt.D1Response += D1Response;
 mqtt.D1Readings += D1Readings;
-JSON_Response d1Response = new JSON_Response();
-JSON_D1Readings d1Readings = new JSON_D1Readings();
+JSON_Response d1Response = new();
+JSON_D1Readings d1Readings = new();
 bool connected = false;
 
-   
+
 connected = await mqtt.StartClient();
 
-if(connected) Console.WriteLine("Type in a command to view an item that was subscribed to or publishing a command to the D1 App.\n " +
-                                "Options for commands are: hide, show, readings, connected"+" \n");
+if (connected) Console.WriteLine("Type in a command to view an item that was subscribed to or publishing a command to the D1 App.\n " +
+                                "Options for commands are: hide, show, readings, connected" + " \n");
 while (connected)
 {
-    string input = Console.ReadLine();
-    switch (input)
-    {
-        case "hide":
-            JSON_Command hide = new JSON_Command
-            {
-                timestamp = DateTime.Now,
-                command_string = "hide"
-            };
-            var hideJSON = JsonConvert.SerializeObject(hide);
-            await mqtt.MqttPublish("d1-command", hideJSON);
-            break;
-        case "show":
-            JSON_Command show = new JSON_Command
-            {
-                timestamp = DateTime.Now,
-                command_string = "show"
-            };
-            var showJSON = JsonConvert.SerializeObject(show);
-            await mqtt.MqttPublish("d1-command", showJSON);
-            break;
-        case "readings":
-            Console.Write("This is the left temp but in the code you can change it to something else. Left Temp:" + d1Readings.left_temperature_raw +" \n");
-            break;
-        case "connected":
-            if(d1Response.command_string != null) Console.Write("The MQTT is connected? "+ d1Response.response_string);
-            else Console.WriteLine("There was nothing published for us to view");
-            break;
-        default:
-            Console.WriteLine("Options for commands are: hide, show, readings, connected"+" \n");
-            break;
-    }
-    connected = mqtt.GetConnection();
+  string input = Console.ReadLine();
+  switch (input)
+  {
+    case "hide":
+      JSON_Command hide = new()
+      {
+        timestamp = DateTime.Now,
+        command_string = "hide"
+      };
+      var hideJSON = JsonSerializer.Serialize(hide);
+      await mqtt.MqttPublish("d1-command", hideJSON);
+      break;
+    case "show":
+      JSON_Command show = new()
+      {
+        timestamp = DateTime.Now,
+        command_string = "show"
+      };
+      var showJSON = JsonSerializer.Serialize(show);
+      await mqtt.MqttPublish("d1-command", showJSON);
+      break;
+    case "readings":
+      Console.Write("This is the left temp but in the code you can change it to something else. Left Temp:" + d1Readings.left_temperature_raw + " \n");
+      break;
+    case "connected":
+      if (d1Response.command_string != null) Console.Write("The MQTT is connected? " + d1Response.response_string);
+      else Console.WriteLine("There was nothing published for us to view");
+      break;
+    default:
+      Console.WriteLine("Options for commands are: hide, show, readings, connected" + " \n");
+      break;
+  }
+  connected = mqtt.GetConnection();
 }
 
 Console.WriteLine("Disconnected");
 
 
- void D1Readings(object sender, JSON_D1Readings thisReading)
+void D1Readings(object sender, JSON_D1Readings thisReading)
 {
-    d1Readings = thisReading;
+  d1Readings = thisReading;
 }
 
- void D1Response(object sender, JSON_Response thisResponse)
- {
-     d1Response = thisResponse;
- }
+void D1Response(object sender, JSON_Response thisResponse)
+{
+  d1Response = thisResponse;
+}
